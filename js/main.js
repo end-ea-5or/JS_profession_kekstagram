6d56aeb8d7f532f52d11ea7bdf6653dd15686864
Andrey <marsel20002008@yandex.ru>
2023-03-17 12:24:41 +0300

    Create two functions-utilities

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log("hello");
+// функция для определения случайного целого (по умолчанию) значения из заданного диапазона
+// для определения случайного значения с плавающей точкой третьим параметром
+// необходимо указывать количество знаков после запятой
+const getRandomIntFromRange = (from, to, decimalPlaces = 0) => {
+  const lower = Math.min(from, to);
+  const upper = Math.max(from, to);
+  return +(Math.random() * (upper - lower) + lower).toFixed(decimalPlaces);
+};
+
+// Функция для проверки максимальной длины строки, равна ли она указанной длине
+const isStringLengthMatches = (str, length) => (str.length === length);
+
+getRandomIntFromRange(1, 100);
+isStringLengthMatches('строка', 6);
