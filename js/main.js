78ff2e0745282dcbf79f83f5e2236bf3c4fe6bca
Andrey <marsel20002008@yandex.ru>
2023-06-19 10:50:22 +0300

    code improvement

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
-import './rendering.js';
+import { simularPhotos } from './data.js';
+import { renderPictures } from './render-pictures.js';
+
+renderPictures(simularPhotos);
---
+++ js/render-pictures.js
@@ -0,0 +1,20 @@
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const containerPictures = document.querySelector('.pictures');
+
+const createPictureElement = (picture) => {
+  const photoElem = pictureTemplate.cloneNode(true);
+  photoElem.querySelector('.picture__img').src = picture.url;
+  photoElem.querySelector('.picture__likes').textContent = picture.likes;
+  photoElem.querySelector('.picture__comments').textContent = picture.comments.length + 1;
+  return photoElem;
+};
+
+const renderPictures = (pictures) => {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((el) => {
+    fragment.append(createPictureElement(el));
+  });
+  containerPictures.append(fragment);
+};
+
+export { renderPictures };
